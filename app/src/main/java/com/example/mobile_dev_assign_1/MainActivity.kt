f83c7e300594f3909e22372e4b34613f12c8b80c
Melania Chiru <melania@chiru.ca>
2025-09-07 15:33:07 -0400

    add attributions for menu item images

--- app/src/main/java/com/example/mobile_dev_assign_1/MainActivity.kt
+++ app/src/main/java/com/example/mobile_dev_assign_1/MainActivity.kt
@@ -233,3 +233,13 @@ fun GreetingPreview() {
         MenuApp()
     }
 }
+
+/*
+* Menu item images attributions
+* Bouillabaisse: https://www.vecteezy.com/vector-art/56659289-a-tropical-shrimp-delight(Ai Graphic from Vecteezy)
+* Boeuf Bourguignon: https://www.vecteezy.com/vector-art/50049543-delicious-beef-bourguignon-stew-with-wine-carrots-and-onion-garnished-with-parsley-creative-banner (Maksym Boiko from Vecteezy)
+* Coq au Vin: https://www.vecteezy.com/vector-art/17406545-yeyuk-bokeum-korean-food (Luz Eugenia Velasquez from Vecteezy)
+* Ratatouille: https://www.vecteezy.com/vector-art/47299077-ratatouille-illustration-design (Fitri Handayani from Vecteezy)
+* Cassoulet: https://www.vecteezy.com/vector-art/51581418-a-bowl-of-curry-with-vegetables-and-herbs-on-a-plate (H M Shamim Al Razi From Vecteezy)
+* Crème Brûlée: https://www.vecteezy.com/vector-art/69742509-caramel-flan-a-delightful-dessert-experience (Nihal Khan from Vecteezy)
+*/
