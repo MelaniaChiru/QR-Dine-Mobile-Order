a2365cff679374fae25e5500bc944a74b4c6fe08
Melania Chiru <melania@chiru.ca>
2025-09-07 12:16:47 -0400

    add main components in a column, and give it a backgrounf colour

--- app/src/main/java/com/example/mobile_dev_assign_1/MainActivity.kt
+++ app/src/main/java/com/example/mobile_dev_assign_1/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -12,6 +14,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -35,9 +38,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MenuApp(modifier: Modifier = Modifier) {
-    Header(modifier = modifier)
-    MenuItemsList(modifier = modifier)
-    CheckoutSection(modifier = modifier)
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFF7FBFE))
+    ){
+        Header(modifier = modifier)
+        MenuItemsList(modifier = modifier)
+        CheckoutSection(modifier = modifier)
+    }
 }
 
 @Composable
