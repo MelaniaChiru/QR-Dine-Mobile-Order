55d7ca838092fe1958bc078188416536e6985ac2
Melania Chiru <melania@chiru.ca>
2025-09-07 12:10:56 -0400

    define base Composables

--- app/src/main/java/com/example/mobile_dev_assign_1/MainActivity.kt
+++ app/src/main/java/com/example/mobile_dev_assign_1/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mobile_dev_assign_1.ui.theme.Mobiledevassign1Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +24,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Mobiledevassign1Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    MenuApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +34,53 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MenuApp(modifier: Modifier = Modifier) {
+    Header(modifier = modifier)
+    MenuItemsList(modifier = modifier)
+    CheckoutSection(modifier = modifier)
 }
 
-@Preview(showBackground = true)
+@Composable
+fun Header(modifier: Modifier = Modifier){
+    Image(
+        painter = painterResource(R.drawable.logo),
+        contentDescription = null,
+        Modifier.width(110.dp)
+    )
+
+    // Add text composable that shows how many items in cart
+    // have a "clear cart" button that resets item count and price
+}
+
+@Composable
+fun MenuItemsList(modifier: Modifier = Modifier){
+//    A list of all the menu items
+//    A column looping though the String Array of menu items
+
+}
+
+@Composable
+fun MenuItem(
+    title: String,
+    price: Double,
+    description: String,
+    imageSrc: String
+){
+    // represents 1 single menu item
+}
+
+@Composable
+fun CheckoutSection(modifier: Modifier = Modifier){
+//    Display total and taxes
+//    Have place order button
+//    eventually show QR code when button is clicked
+
+}
+
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     Mobiledevassign1Theme {
-        Greeting("Android")
+        MenuApp()
     }
 }
