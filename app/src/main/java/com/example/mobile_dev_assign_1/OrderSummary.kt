82ffa5da0b531d790d436607023fa8c5f2200126
Melania Chiru <melania@chiru.ca>
2025-09-10 14:38:02 -0400

    remove unused dataclass

--- app/src/main/java/com/example/mobile_dev_assign_1/OrderSummary.kt
+++
@@ -1,9 +0,0 @@
-package com.example.mobile_dev_assign_1
-
-data class OrderSummary(
-    val items: List<MenuItem>,
-    val subtotal: Double,
-    val gst: Double,
-    val qst: Double,
-    val total: Double
-)
